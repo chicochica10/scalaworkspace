9c0844f7ae622589d20af267a6bba6526c1cda94
chicochica10 <chicochica10@gmail.com>
2013-10-14 15:57:35 +0200

    codebits

--- 4-patmat/src/main/scala/patmat/Huffman.scala
+++ 4-patmat/src/main/scala/patmat/Huffman.scala
@@ -350,7 +350,10 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = table match  {
+     case Nil => Nil
+     case (x,lb)::xs => if (x==char) lb else codeBits (xs)(char)
+  }
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
